7f03745c98a09bd88f9769164bb664514d04c66f
Ben Tien <benjamin.tien@flatironschool.com>
2018-12-29 08:17:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    print "FizzBuzz"
+    return "FizzBuzz"
   elsif int % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif int % 5 == 0 
-    puts "Buzz"
+    return "Buzz"
   end
 end
