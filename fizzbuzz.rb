e20b3a2e95824f22727cd470ad7ab3b7c290ce32
Ben Tien <benjamin.tien@flatironschool.com>
2018-12-29 08:05:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,8 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     print "FizzBuzz"
-  #elsif int % 3 == 0
-  #  puts "Fizz"
-  #elsif int % 5 == 0 
-  #  puts "Buzz"
-  #else puts "nil"
+  elsif int % 3 == 0
+    puts "Fizz"
+  else int % 5 == 0 
+    puts "Buzz"
 end
